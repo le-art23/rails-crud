894fdfa2a1631582b5bb616c1e39dd3960182e0e
leaneartis <leane.artis@icloud.com>
2024-09-01 16:37:56 -0400

    Fichers lecture models&CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Read all
+  get "restaurants", to: "restaurants#index"
+  # Create
+  get "restaurants/new", to: "restaurants#new"
+  post "restaurants", to: "restaurants#create"
+  # Read one
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # Update
+  get "restaurants/:id/edit", to: "restaurants#edit"
+  patch "restaurants/:id", to: "restaurants#update"
+  # Delete
+  delete "restaurants/:id", to: "restaurants#destroy"
 end
